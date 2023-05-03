ae31a1b6195e083986b8945126c95da575b08d5a
Anthony Tiu <helloworldpns@gmail.com>
2023-05-03 19:58:17 +0800

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  counter = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) {
+      counter += string;
+    }
+    return counter;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
