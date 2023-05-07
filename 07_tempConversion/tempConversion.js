915ac6f07d81db236bb7299216c73ddbc5b57737
Anthony Tiu <helloworldpns@gmail.com>
2023-05-07 23:38:39 +0800

    added tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let value1 = 0;
+  value1 = ((fahrenheit - 32) * 5) / 9;
+  return Math.round(value1 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let value2 = 0;
+  value2 = (celsius * 9) / 5 + 32;
+  return Math.round(value2 * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
