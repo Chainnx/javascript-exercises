ef2a3e7ea102c2749aeb6367a5e60f3c66b43c7b
Anthony Tiu <helloworldpns@gmail.com>
2023-05-07 15:03:28 +0800

    added sumALl and leapYears

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+function sumAll(num1, num2) {
+  let sum = 0;
+  if (typeof num2 === "number" && typeof num1 === "number") {
+    if (num1 < num2 && Math.sign(num1) === 1) {
+      for (let i = 0; num1 <= num2; i++, num1++) {
+        sum = sum + num1;
+      }
+      return sum;
+    } else if (num1 > num2 && Math.sign(num2) === 1) {
+      for (let i = 0; num1 >= num2; i++, num2++) {
+        sum = sum + num2;
+      }
+      return sum;
+    } else {
+    }
+    return "ERROR";
+  } else {
+    return "ERROR";
+  }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
